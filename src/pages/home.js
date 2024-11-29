96f854fccc4c86b71dda5138ad4a6608db4c51b7
Mekasu <mekasurenae@gmail.com>
2024-11-29 10:27:00 -0600

    built home screen

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,76 @@
+import { useEffect, useState } from "react";
+import {
+  HomeContainer,
+  TitleContainer,
+  Title,
+  DiaryList,
+  NewEntryButton,
+  ListItem,
+  ListItemTopRow,
+  ListItemBottomRow,
+  ListItemTitle,
+  ListItemText
+} from "../styles/pages/home.style";
+
 export default function Home() {
+  const [entries, setEntries] = useState(null);
+
+  // useEffect(() => {
+  //   if (!entries || entries.length === 0) {
+  //     setEntries([]);
+  //   }
+  // }, [entries]);
+
+  const generateEntries = () => {
+    if (!entries) {
+      return (
+        <ListItem>
+          <ListItemTopRow>
+            <ListItemTitle>
+              No Entries Found
+            </ListItemTitle>
+          </ListItemTopRow>
+
+          <ListItemBottomRow>
+            <ListItemText>
+              Click the "New Entry" Button
+              Below To Get Started!
+            </ListItemText>
+          </ListItemBottomRow>
+        </ListItem>
+      );
+    };
+
+    return entries.map(entry => {
+      return (
+        <ListItem key={entry.id}>
+          <ListItemTopRow>
+            <ListItemText>{entry.date}</ListItemText>
+            <ListItemTitle>{entry.title}</ListItemTitle>
+            <ListItemText>{entry.time}</ListItemText>
+          </ListItemTopRow>
+
+          <ListItemBottomRow>
+            <ListItemText>{entry.entry.substring(0, 50) + "..."}</ListItemText>
+          </ListItemBottomRow>
+        </ListItem>
+      );
+    });
+  };
+
   return (
-    <div>Home Screen</div>
+    <HomeContainer>
+      <TitleContainer>
+        <Title>
+          Mek&#39;s Diary
+        </Title>
+      </TitleContainer>
+
+      <DiaryList>{ generateEntries() }</DiaryList>
+
+      <NewEntryButton to="/new-entry">
+        New Entry
+      </NewEntryButton>
+    </HomeContainer>
   );
 };
