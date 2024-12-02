789a18ed848e397c1ca5cc63f49d1a0204c512a6
Mekasu <mekasurenae@gmail.com>
2024-12-02 03:15:13 -0600

    updated routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import { Routes, Route } from 'react-router-dom';
 
-import { GlobalStyles } from './styles/globalStyles';
 import Home from './pages/home';
+
+// auth routes
+import SignUp from './pages/auth/signup';
+import Login from './pages/auth/login';
+
 import NewEntry from './pages/newEntry';
 
 
 export default function App() {
   return (
     <div className="App">
-      <GlobalStyles />
-      
       <Routes>
         <Route path="/" element={<Home />} />
+
+        <Route path="/auth">
+          <Route path="signup" element={<SignUp />} />
+          <Route path="login" element={<Login />} />
+        </Route>
+
         <Route path="/new-entry" element={<NewEntry />} />
       </Routes>
     </div>
